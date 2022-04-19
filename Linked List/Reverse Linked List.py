7990f341f5f88247319dabcc0c19079a8555584d
kim hyeonuk <scar@ugjjang-ui-maegbug.local>
2022-04-19 16:45:09 +0900

    solve Linked List/Reverse Linked List.py by rescursively solution but code is so ugly

--- Linked List/Reverse Linked List.py
+++ Linked List/Reverse Linked List.py
@@ -1,4 +1,6 @@
 # Definition for singly-linked list.
+from math import factorial
+from subprocess import call
 from typing import Optional
 
 
@@ -33,14 +35,55 @@ class ListNode:
 # Iteratively Pretty Solution #
 # Trying by reference #
 
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         before = None        
+        
+#         while head is not None:                          
+#             after = head.next
+#             head.next=before
+#             before = head
+#             head = after
+
+#         return before
+
+
+# Recursively Solution #
+# First trying#
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        before = None        
-        
-        while head is not None:                          
-            after = head.next
-            head.next=before
-            before = head
-            head = after
+        result = self.rec(head,None)                 
+        return result[1]
 
-        return before
+    def rec(self,head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            cursor = head
+            return head, cursor
+
+        else :                            
+            caller = head
+            callee = self.rec(head.next,None)    
+            callee.next=caller
+
+            caller.next=None
+            return caller, cursor
+
+# Recursively solution #
+# reference code #
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next=None
+        return newHead
+
+# class Rec:
+#     def factorial(self, n)->int:
+#         if n == 1:
+#             return n
+#         else :
+#             return n * self.factorial(n-1)
