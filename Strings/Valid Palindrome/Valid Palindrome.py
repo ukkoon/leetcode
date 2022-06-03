880c02d8afe7eac4b2399553def07ce3d47491dc
kim hyeonuk <scar@ugjjang-ui-maegbug.local>
2022-06-03 15:47:47 +0900

    optimize Valid Palindrome

--- Strings/Valid Palindrome/Valid Palindrome.py
+++ Strings/Valid Palindrome/Valid Palindrome.py
@@ -2,14 +2,11 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        letters = "".join(re.findall("[a-zA-Z0-9]", s))
-        n = len(letters)
-        print(letters)
+        letters = "".join(re.findall("[a-zA-Z0-9]", s)).lower()
+        n = len(letters)        
         for i in range(0,(n//2)):
-            if letters[i].upper() != letters[n-i-1].upper():
+            if letters[i] != letters[n-i-1]:
                 return False
         return True
-            
-
 
 print(Solution().isPalindrome(s="0P"))
