4e711459afff60cedaa815a31c235f0b2a9f458f
Ubuntu <ubuntu@ip-172-31-15-244.ap-northeast-2.compute.internal>
2022-11-04 16:41:09 +0000

    solved generate parenthese using sliding window technic

--- Recursion/generate_parentheses.py
+++ Recursion/generate_parentheses.py
@@ -1,5 +1,5 @@
-# set을 이용한 방법 (n^2)
 class Solution:
+    # set을 이용한 방법 (n^2)
     def lengthOfLongestSubstring(self, s: str) -> int:        
         longestLen = 0
         for i in range(len(s)):
@@ -13,4 +13,18 @@ class Solution:
 
         return longestLen
 
-# Sliding Window technic
+    # Sliding Window technic
+    def lengthOfLongestSubstring2(self, s:str) -> int:
+        
+        longest = cur = s[0] if s else ""
+
+        for i in range(len(s)-1):
+            if s[i+1] not in cur:
+                cur+=s[i+1]
+            else:
+                cur = cur[cur.index(s[i+1])+1:]+s[i+1]
+            if len(cur)>len(longest):
+                longest=cur
+
+        return len(longest)
+
